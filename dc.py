42f526cc8d23f0452c2db2fe2a18cb37ca4783e4
imbinnat <154840036+imbinnat@users.noreply.github.com>
2024-01-03 09:42:05 +0300

    Add files via upload

---
+++ dc.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+# Tkinter penceresini oluştur
+root = tk.Tk()
+root.title("123")
+
+# Butonu oluştur
+button = tk.Button(root, text="btn")
+button.pack(pady=20)
+
+# Tkinter penceresini başlat
+root.mainloop()
